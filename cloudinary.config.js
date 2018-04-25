db4d569ff92d82ee448eac9224ae8d218e3b6f20
Fabi <fsanz@tutamail.com>
2018-04-25 13:43:17 -0300

    hide config

--- cloudinary.config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var cloudinary = require('cloudinary');
-const cloudinary_config = cloudinary.config({ 
-    cloud_name: 'dftu7s8cf', 
-    api_key: '479194718319492', 
-    api_secret: 'ymVJcMAkDYClt_aiZk3nNY8mMno' 
-  });
-module.exports = cloudinary_config;
--- config.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = {
-    port : process.env.PORT || 3800,
-    db : process.env.MONGODB_URI || 'mongodb://localhost:27017/mean_social',
-    SECRET_TOKEN: 'clave_secreta_red_social_node',
-}
